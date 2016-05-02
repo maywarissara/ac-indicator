516ddbf6f676fdb706f279a11c1c28aa7aa480a3
maywarissara <wphupai1@asu.edu>
2016-05-02 12:34:21 -0700

    changed

--- src/ai.js
+++ src/ai.js
@@ -1,6 +1,13 @@
 var AI = {
-  show: function(){
+  show: function(style){
+  	var styleTest = style;
+  	if (styleTest === "apple") {
+    var mu = "<div class='fadeBox'> <div class='apple'> </div> </div>";
+   } else if (styleTest === "circle") {
+    var mu = "<div class='fadeBox'> <div class='circle'> </div> </div>";
+	 } else {
     var mu = "<div class='fadeBox'> <div class='spinner'> </div> </div>";
+  }
     document.body.innerHTML += mu;
   },
   hide: function(){
